c8f7525a5aeb6c3cfe3ff7421382b61de35b960b
amirelshahbour-afk <amirelshahbour@gmail.com>
2026-09-14 17:16:14 +0300

    Improve fitness logging, editable records, goals and backups

--- sw.js
+++ sw.js
@@ -1,4 +1,2 @@
-const CACHE='fitness-tracker-v1';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(caches.delete('fitness-tracker-v1').then(()=>self.registration.unregister())));
